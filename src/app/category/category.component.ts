581becc21d5e73cc398ab45c1e03e40803d809dc
Jaydip Patel <jaydip@qseksolutions.com>
2018-05-10 14:14:11 +0530

    category pagination and changes

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -23,6 +23,11 @@ export class CategoryComponent implements OnInit {
   public base_url: any = myGlobals.base_url;
   chart: Chart;
   coindata: any;
+  cuurentpage: any = 0;
+  nextpage: any = 0;
+  prevpage: any = 0;
+  pagecount: any = 0;
+  showloader: any;
   categorylist: any;
   categorydata: any;
   totalcategory: any;
@@ -31,6 +36,7 @@ export class CategoryComponent implements OnInit {
   favoritedata: any = [];
   favorites: any = [];
   i: any = 0;
+  start: any = 0;
   displaycolumn: any = ['rank', 'name', 'follow', 'price_usd', 'graph_7d', 'mc_usd', 'team', 'theory', 'technology', 'traction', 'tam', 'token', 'timing', 'trasformative', 'gq'];
 
   constructor(private coinservice: CoinService, private router: Router, toasterService: ToasterService, private title: Title, private meta: Meta, private decimalpipe: DecimalPipe) {
@@ -160,16 +166,37 @@ export class CategoryComponent implements OnInit {
         this.totalcategory = resData.data.length;
       }
     });
-    this.gettabledata();
+    this.gettabledata(this.start);
+    this.cuurentpage = 1;
   }
 
-  gettabledata() {
+  gotonextpage() {
+    this.start = this.start + 50;
+    this.cuurentpage = this.cuurentpage + 1;
+    this.gettabledata(this.start);
+  }
+  
+  gotoprevpage() {
+    this.start = this.start - 50;
+    this.cuurentpage = this.cuurentpage - 1;
+    this.gettabledata(this.start);
+  }
+
+  gettabledata(start) {
+    this.showloader = true;
     let curl = window.location.pathname;
     let spliturl = curl.split('/');
     let category = spliturl[2];
-    this.coinservice.getcategorywisedata(category).subscribe(responce => {
+    this.coinservice.getcategorywisedata(category, start).subscribe(responce => {
+      console.log(responce);
       if (responce.status === true) {
+        this.showloader = false;
         this.coindata = responce.data;
+        setTimeout(() => {
+          $('.sparkliness1').sparkline('html', { lineWidth: 2, disableInteraction: true, spotColor: false, minSpotColor: false, maxSpotColor: false, width: 150, lineColor: '#04b290', height: 30, fillColor: '#ffffff' });
+        }, 1000);
+        let totalpage = responce.totalCount / 50;
+        this.pagecount = Math.ceil(totalpage);
         /* this.coinservice.getalldatafromjson().subscribe(resData => {
           this.categorydata = responce.data;
           if (resData.status === true) {
@@ -187,95 +214,10 @@ export class CategoryComponent implements OnInit {
           }
         }); */
       }
-    });
-
-    this.chart = new Chart({
-      chart: {
-        type: 'spline',
-        margin: [0, 0, 0, 0],
-        backgroundColor: 'transparent',
-        width: 150,
-        height: 25,
-      },
-      title: {
-        text: ''
-      },
-      credits: {
-        enabled: false
-      },
-      xAxis: {
-        labels: {
-          enabled: false
-        },
-        minorGridLineWidth: 0,
-        lineColor: 'transparent',
-        title: {
-          text: null
-        },
-        startOnTick: false,
-        endOnTick: false,
-        tickPositions: [],
-      },
-      yAxis: {
-        endOnTick: false,
-        startOnTick: false,
-        lineWidth: 0,
-        lineColor: 'transparent',
-        labels: {
-          enabled: false
-        },
-        title: {
-          text: null
-        },
-        tickPositions: []
-      },
-      legend: {
-        enabled: false
-      },
-      tooltip: {
-        shadow: false,
-        useHTML: true,
-        hideDelay: 0,
-        shared: true,
-        borderWidth: 1,
-        padding: 5,
-        backgroundColor: '#ffffff',
-        headerFormat: '',
-        pointFormat: '<b style="z-index : 999999">${point.y}</b>',
-        positioner: function (w, h, point) {
-          return { x: point.plotX - w / 2, y: point.plotY - h };
-        }
-      },
-      plotOptions: {
-        series: {
-          animation: false,
-          lineWidth: 2,
-          shadow: false,
-          color: '#00362d',
-          states: {
-            hover: {
-              lineWidth: 3
-            }
-          },
-          marker: {
-            radius: 1,
-            states: {
-              hover: {
-                radius: 2
-              }
-            }
-          },
-        },
-        column: {
-          negativeColor: '#000000',
-          borderColor: '#000000'
-        }
-      },
-      exporting: { enabled: false },
-      series: [{
-        name: '$',
-        data: [10, 5, 15, 16, 24, 20, 30, 32, 33, 35, 33, 40, 71, 78, 39, 66],
-      }],
+      else {
+        this.showloader = false;
+        this.coindata = '';
+      }
     });
 
     $(document).on('click', '.showmore', function () {
@@ -298,13 +240,13 @@ export class CategoryComponent implements OnInit {
   }
 
   errorHandler(event, name) {
-    const imgurl = 'assets/currency-25/' + name.toLowerCase() + '.png';
+    const imgurl = 'assets/images/currency-25/' + name.toLowerCase() + '.png';
     this.isImage(imgurl).then(function (test) {
       // tslint:disable-next-line:triple-equals
       if (test == true) {
         return event.target.src = imgurl;
       } else {
-        return event.target.src = 'assets/currency-25/not-found-25.png';
+        return event.target.src = 'assets/images/currency-25/not-found-25.png';
       }
     });
   }
