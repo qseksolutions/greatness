0f29d5f6e5f24fe97acbc2bf1b26a2a4b870369a
DESKTOP-87P3H06\Kaushik <kaushik@qseksolutions.com>
2018-05-15 13:08:11 +0530

    range-slider(kaushik)

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -21,14 +21,117 @@ $(function () {
   $('.selectpicker').select2()
   $('[data-toggle="tooltip"]').tooltip()
 });
+$("#Circ_Supply").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 8.35,
+    from: 0,
+    to: 8.35,
+    prefix: "Tn"
+});
+$("#Circ_Supply").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 8.35,
+    from: 0,
+    to: 8.35,
+    prefix: "Tn"
+});
+$("#Max_Supply").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 8.00,
+    from: 0,
+    to: 8.00,
+    prefix: "Tn"
+});
 
-$('.slider-input').jRange({
+$("#gq").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 100,
     from: 0,
     to: 100,
-    step: 1,
-    scale: [0,100],
-    format: '%s',
-    width: 425,
-    showLabels: true,
-    isRange : true
+    prefix: "%"
 });
+$("#team_expe").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 100,
+    from: 0,
+    to: 100,
+    prefix: "%"
+});
+$("#Theory").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 100,
+    from: 0,
+    to: 100,
+    prefix: "%"
+});
+$("#Tech").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 100,
+    from: 0,
+    to: 100,
+    prefix: "%"
+});
+$("#Traction").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 100,
+    from: 0,
+    to: 100,
+    prefix: "%"
+});
+$("#tam").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 100,
+    from: 0,
+    to: 100,
+    prefix: "%"
+});
+$("#Token").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 100,
+    from: 0,
+    to: 100,
+    prefix: "%"
+});
+$("#Timing").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 100,
+    from: 0,
+    to: 100,
+    prefix: "%"
+});
+$("#Transformative").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 100,
+    from: 0,
+    to: 100,
+    prefix: "%"
+});
+$("#Market_cap").ionRangeSlider({
+    type: "double",
+    min: 0.00,
+    max: 185.37,
+    from: 0,
+    to: 185.37,
+    prefix: "$"
+});
+$("#coin_age").ionRangeSlider({
+    type: "double",
+    min: 0.00,
+    max: 48.3,
+    from: 0,
+    to: 48.3,
+    prefix: "$"
+});
+
